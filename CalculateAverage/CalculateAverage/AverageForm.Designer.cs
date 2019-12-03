404d22cb7071aa4591ce35775aa87cf0881ebecd
Kaelan Khan <32547155+KaelanKhan@users.noreply.github.com>
2019-12-03 13:10:07 -0500

    calculate average

---
+++ CalculateAverage/CalculateAverage/AverageForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace CalculateAverage
+{
+    partial class frmAverage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstMarks = new System.Windows.Forms.ListBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtMark = new System.Windows.Forms.TextBox();
+            this.lblEnter = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstMarks
+            // 
+            this.lstMarks.FormattingEnabled = true;
+            this.lstMarks.Location = new System.Drawing.Point(12, 125);
+            this.lstMarks.Name = "lstMarks";
+            this.lstMarks.Size = new System.Drawing.Size(189, 186);
+            this.lstMarks.TabIndex = 0;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(38, 317);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(128, 41);
+            this.btnCalculate.TabIndex = 1;
+            this.btnCalculate.Text = "Calculate Average";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(35, 381);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(25, 13);
+            this.lblOutput.TabIndex = 2;
+            this.lblOutput.Text = "???";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(56, 81);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 24);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Enter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // txtMark
+            // 
+            this.txtMark.Location = new System.Drawing.Point(118, 42);
+            this.txtMark.Name = "txtMark";
+            this.txtMark.Size = new System.Drawing.Size(100, 20);
+            this.txtMark.TabIndex = 4;
+            // 
+            // lblEnter
+            // 
+            this.lblEnter.AutoSize = true;
+            this.lblEnter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnter.Location = new System.Drawing.Point(12, 42);
+            this.lblEnter.Name = "lblEnter";
+            this.lblEnter.Size = new System.Drawing.Size(100, 20);
+            this.lblEnter.TabIndex = 5;
+            this.lblEnter.Text = "Enter a mark";
+            // 
+            // frmAverage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblEnter);
+            this.Controls.Add(this.txtMark);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lstMarks);
+            this.Name = "frmAverage";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstMarks;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtMark;
+        private System.Windows.Forms.Label lblEnter;
+    }
+}
+
---
+++ CalculateAverage/CalculateAverage/AverageForm.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculateAverage
+{
+    public partial class frmAverage : Form
+    {
+        // create global list
+        List<double> myListOfIntegers = new List<double>();
+
+        public frmAverage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            // declare local vars
+            double average = 0;
+            int
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            // declare local vars
+            int userMark = -1;
+
+            // convert the value in the textbox to an int
+            try
+            {
+                userMark = int.Parse(txtMark.Text);
+            }
+            catch (Exception parseError)
+            {
+                Console.WriteLine("An error has occurred: '{0}'", parseError);
+            }
+
+            // check if the user entered a number between 0 and 100
+            if ( (userMark >= 0) && (userMark <= 100))
+            {
+                // add the mark to the listbox
+                this.lstMarks.Items.Add(userMark);
+
+                // add the mark to the list
+                myListOfIntegers.Add(userMark);
+            }
+            // otherwise, display an error
+            else
+            {
+                MessageBox.Show("Please enter a number between 0 and 100.", "Invalid Number");
+            }
+        }
+    }
+}
