649bbcd0cbe612c2d61c629868a2e626356aa829
Kaelan Khan <32547155+KaelanKhan@users.noreply.github.com>
2019-12-04 13:10:54 -0500

    deal card

---
+++ dealCard/dealCard/dealCardForm.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace dealCard
+{
+    partial class dealCardForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picCard = new System.Windows.Forms.PictureBox();
+            this.btnDeal = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picCard)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picCard
+            // 
+            this.picCard.Image = global::dealCard.Properties.Resources.QS;
+            this.picCard.Location = new System.Drawing.Point(38, 12);
+            this.picCard.Name = "picCard";
+            this.picCard.Size = new System.Drawing.Size(186, 261);
+            this.picCard.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picCard.TabIndex = 0;
+            this.picCard.TabStop = false;
+            // 
+            // btnDeal
+            // 
+            this.btnDeal.Location = new System.Drawing.Point(84, 299);
+            this.btnDeal.Name = "btnDeal";
+            this.btnDeal.Size = new System.Drawing.Size(91, 34);
+            this.btnDeal.TabIndex = 1;
+            this.btnDeal.Text = "Deal Card";
+            this.btnDeal.UseVisualStyleBackColor = true;
+            this.btnDeal.Click += new System.EventHandler(this.BtnDeal_Click);
+            // 
+            // dealCardForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(270, 450);
+            this.Controls.Add(this.btnDeal);
+            this.Controls.Add(this.picCard);
+            this.Name = "dealCardForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picCard)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picCard;
+        private System.Windows.Forms.Button btnDeal;
+    }
+}
+
---
+++ dealCard/dealCard/dealCardForm.cs
@@ -0,0 +1,107 @@
+﻿/*
+ * Created by: Kaelan K
+ * Created on: 04-12-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #39 - Deal Card
+ * This program...
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dealCard
+{
+    public partial class dealCardForm : Form
+    {
+        //create an empty list
+        List<Image> listCardImages = new List<Image>();
+
+        public dealCardForm()
+        {
+            InitializeComponent();
+        }
+
+        private int DealCard(ref List<Image> cardImages)
+        {
+
+        }
+
+        private void NewDeck()
+        {
+            listCardImages.Add(Properties.Resources.AC);
+            listCardImages.Add(Properties.Resources.AD);
+            listCardImages.Add(Properties.Resources.AH);
+            listCardImages.Add(Properties.Resources.AS);
+            listCardImages.Add(Properties.Resources.KC);
+            listCardImages.Add(Properties.Resources.KD);
+            listCardImages.Add(Properties.Resources.KH);
+            listCardImages.Add(Properties.Resources.KS);
+            listCardImages.Add(Properties.Resources.QC);
+            listCardImages.Add(Properties.Resources.QD);
+            listCardImages.Add(Properties.Resources.QH);
+            listCardImages.Add(Properties.Resources.QS);
+            listCardImages.Add(Properties.Resources.JC);
+            listCardImages.Add(Properties.Resources.JD);
+            listCardImages.Add(Properties.Resources.JH);
+            listCardImages.Add(Properties.Resources.JS);
+            listCardImages.Add(Properties.Resources._10C);
+            listCardImages.Add(Properties.Resources._10D);
+            listCardImages.Add(Properties.Resources._10H);
+            listCardImages.Add(Properties.Resources._10S);
+            listCardImages.Add(Properties.Resources._9C);
+            listCardImages.Add(Properties.Resources._9D);
+            listCardImages.Add(Properties.Resources._9H);
+            listCardImages.Add(Properties.Resources._9S);
+            listCardImages.Add(Properties.Resources._8C);
+            listCardImages.Add(Properties.Resources._8D);
+            listCardImages.Add(Properties.Resources._8H);
+            listCardImages.Add(Properties.Resources._8S);
+            listCardImages.Add(Properties.Resources._7C);
+            listCardImages.Add(Properties.Resources._7D);
+            listCardImages.Add(Properties.Resources._7H);
+            listCardImages.Add(Properties.Resources._7S);
+            listCardImages.Add(Properties.Resources._6C);
+            listCardImages.Add(Properties.Resources._6D);
+            listCardImages.Add(Properties.Resources._6H);
+            listCardImages.Add(Properties.Resources._6S);
+            listCardImages.Add(Properties.Resources._5C);
+            listCardImages.Add(Properties.Resources._5D);
+            listCardImages.Add(Properties.Resources._5H);
+            listCardImages.Add(Properties.Resources._5S);
+            listCardImages.Add(Properties.Resources._4C);
+            listCardImages.Add(Properties.Resources._4D);
+            listCardImages.Add(Properties.Resources._4H);
+            listCardImages.Add(Properties.Resources._4S);
+            listCardImages.Add(Properties.Resources._3C);
+            listCardImages.Add(Properties.Resources._3D);
+            listCardImages.Add(Properties.Resources._3H);
+            listCardImages.Add(Properties.Resources._3S);
+            listCardImages.Add(Properties.Resources._2C);
+            listCardImages.Add(Properties.Resources._2D);
+            listCardImages.Add(Properties.Resources._2H);
+            listCardImages.Add(Properties.Resources._2S);
+        }
+
+        private void BtnDeal_Click(object sender, EventArgs e)
+        {
+
+            
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
